bad1ea114c19ff76bea0bbc568506288533a69f8
梶塚太智 <kajizukataichi@outlook.jp>
2024-07-30 23:20:30 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use crossterm::{
     cursor,
     style::{PrintStyledContent, Stylize},
-    terminal, QueueableCommand,
+    terminal, ExecutableCommand, QueueableCommand,
+};
+use crossterm::{
+    event::{self, KeyEvent},
+    terminal::{disable_raw_mode, enable_raw_mode},
 };
 use std::io::{self, Write};
+use std::time::Duration;
 use std::{env::args, fs::File, io::read_to_string};
 
 fn main() {
@@ -12,13 +17,16 @@ fn main() {
     slide_show(presen)
 }
 
-/// Get standard input
-fn input(prompt: &str) -> String {
-    print!("{}", prompt);
-    io::stdout().flush().unwrap();
-    let mut result = String::new();
-    io::stdin().read_line(&mut result).ok();
-    result.trim().to_string()
+fn wait_keypress() {
+    let _ = enable_raw_mode();
+    loop {
+        if event::poll(Duration::from_secs(1)).unwrap_or(false) {
+            if let event::Event::Key(KeyEvent { .. }) = event::read().unwrap() {
+                break;
+            }
+        }
+    }
+    let _ = disable_raw_mode();
 }
 
 fn slide_show(presen: String) {
@@ -36,25 +44,41 @@ fn draw_slide(slide: String) -> io::Result<()> {
     let mut line_count = 1;
     for text in slide.split("\n") {
         let text: String = text.to_owned() + "\n";
-
-        let (width, _) = terminal::size()?;
-        let terminal_width = width as usize;
-        let text_length = text.len();
-        let position = (terminal_width - text_length) / 2;
-
-        stdout.queue(cursor::MoveTo(position as u16, line_count))?;
-        if text.starts_with("#") {
-            stdout.queue(PrintStyledContent(
-                text.replacen("#", "", 1)
-                    .attribute(crossterm::style::Attribute::Bold),
-            ))?;
-        } else {
-            stdout.queue(PrintStyledContent(text.with(crossterm::style::Color::Grey)))?;
-        }
-        stdout.flush()?;
+        show_text(text, line_count)?;
         line_count += 1;
-        input("");
+        wait_keypress();
     }
 
+    Ok(())
+}
+fn show_text(text: String, line_count: u16) -> io::Result<()> {
+    let mut stdout = io::stdout();
+    let (width, _) = terminal::size()?;
+    let terminal_width = width as usize;
+    let text_length = text.len();
+    let position = (terminal_width - text_length) / 2;
+
+    stdout.queue(cursor::MoveTo(position as u16, line_count))?;
+    if text.contains("->") {
+        let (before, after) = {
+            let splited = text.split("->").collect::<Vec<&str>>();
+            (
+                splited[0].trim().to_string(),
+                splited[1..splited.len()].join("->").trim().to_string(),
+            )
+        };
+        show_text(before, line_count)?;
+        wait_keypress();
+        stdout.execute(terminal::Clear(terminal::ClearType::CurrentLine))?;
+        show_text(after, line_count)?;
+    } else if text.starts_with("#") {
+        stdout.queue(PrintStyledContent(
+            text.replacen("#", "", 1)
+                .attribute(crossterm::style::Attribute::Bold),
+        ))?;
+    } else {
+        stdout.queue(PrintStyledContent(text.with(crossterm::style::Color::Grey)))?;
+    }
+    stdout.flush()?;
     Ok(())
 }
